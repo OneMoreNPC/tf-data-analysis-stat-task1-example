18b4d380ab4c3607d4ff891c30f6769926b6e6c3
OneMoreNPC <129788997+OneMoreNPC@users.noreply.github.com>
2023-04-07 20:43:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,7 @@
-import pandas as pd
 import numpy as np
-
 
 chat_id = 49479018 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    
-    return (x.mean() + 39) / 10 # Ваш ответ
+    from scipy.stats import expon
+    return (x.mean() - expon.mean(loc=-39)) / 10 # Ваш ответ
